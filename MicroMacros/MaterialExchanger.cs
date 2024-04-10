2d092403bd3075af6d84141768c9283dc24bdbd2
UnknownX7 <UnknownX7@users.noreply.github.com>
2024-04-10 07:37:53 -0500

    Add synthesis material exchange automation support

--- MicroMacros/MaterialExchanger.cs
+++ MicroMacros/MaterialExchanger.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using ECGen.Generated.Command.OutGame.ContentChanger;
 using ECGen.Generated.Command.OutGame.Shop;
 using ECGen.Generated.Command.UI;
@@ -14,18 +15,32 @@ public unsafe class MaterialExchanger : IMicroMacro
     private bool enabled = false;
     public ref bool Enabled => ref enabled;
 
-    private const long exchangeShopId = 206001;
-    private static readonly long[] shopItems =
-    [
-        551000000010011,
-        551000000010012,
-        551000000010021,
-        551000000010022,
-        551000000010041,
-        551000000010042
-    ];
+    private static readonly Dictionary<long, long[]> shopItems = new()
+    {
+        [206001] =
+            [
+                551000000010011,
+                551000000010012,
+                551000000010021,
+                551000000010022,
+                551000000010041,
+                551000000010042
+            ],
+        [206002] =
+            [
+                551000000005011,
+                551000000005021,
+                551000000005031,
+                551000000005041,
+                551000000005051,
+                551000000005061,
+                551000000005091,
+                551000000005111,
+                551000000005121
+            ]
+    };
 
-    private long selectedShopItem = shopItems.First();
+    private long selectedShopItem = 0;
     private int count = 0;
 
     public void Update()
@@ -40,7 +55,7 @@ public unsafe class MaterialExchanger : IMicroMacro
         {
             foreach (var t in shopModal.NativePtr->contentHandles->Enumerable)
             {
-                if (Il2CppType<ShopWork.ShopStore>.Is(t.Item2, out var shopStore) && shopStore->masterShop->id == exchangeShopId
+                if (Il2CppType<ShopWork.ShopStore>.Is(t.Item2, out var shopStore) && shopItems.ContainsKey(shopStore->masterShop->id)
                     && Il2CppType<ContentChanger.ContentHandle>.Is(t.Item1, out var contentHandle)
                     && Il2CppType<ShopMultipleContent>.Is(contentHandle->content, out var shopContent) && shopContent->isOpen && shopContent->shopProductList->scroller->activeCells->size != 0)
                 {
@@ -57,7 +72,7 @@ public unsafe class MaterialExchanger : IMicroMacro
         }
         else if (ModalManager.Instance?.GetCurrentModal<ShopCheckExchangeModalPresenter>() is { } shopCheckExchangeModal)
         {
-            if (shopCheckExchangeModal.NativePtr->currentShopProductParameter->ShopId == exchangeShopId
+            if (shopItems.ContainsKey(shopCheckExchangeModal.NativePtr->currentShopProductParameter->ShopId)
                 && Il2CppType<ShopWork.ShopItemStore>.Is(shopCheckExchangeModal.NativePtr->currentShopProductParameter->ShopItemInfo, out var shopItemStore)
                 && shopItemStore->masterShopItem->nameLanguageId == selectedShopItem
                 && GameInterop.TapButton(shopCheckExchangeModal.NativePtr->view->confirmButton, false))
@@ -76,28 +91,29 @@ public unsafe class MaterialExchanger : IMicroMacro
         var modal = ModalManager.Instance.GetModal<ShopModalPresenter>();
         if (modal == null) return;
 
-        var found = false;
+        var found = 0L;
         foreach (var t in modal.NativePtr->contentHandles->Enumerable)
         {
-            if (!Il2CppType<ShopWork.ShopStore>.Is(t.Item2, out var shopStore) || shopStore->masterShop->id != exchangeShopId) continue;
+            if (!Il2CppType<ShopWork.ShopStore>.Is(t.Item2, out var shopStore) || !shopItems.ContainsKey(shopStore->masterShop->id)) continue;
             if (!Il2CppType<ContentChanger.ContentHandle>.Is(t.Item1, out var contentHandle)
                 || !Il2CppType<ShopMultipleContent>.Is(contentHandle->content, out var shopContent)
                 || !shopContent->isOpen)
-                return;
+                continue;
 
-            found = true;
+            found = shopStore->masterShop->id;
             break;
         }
 
-        if (!found) return;
+        if (found == 0) return;
 
         ImGui.Begin("MaterialExchanger", ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoDecoration);
         if (ImGui.BeginCombo("##Item", GameInterop.GetLocalizedText(LocalizeTextCategory.ShopItem, selectedShopItem)))
         {
-            foreach (var shopItem in shopItems)
+            foreach (var shopItem in shopItems[found])
             {
-                if (ImGui.Selectable(GameInterop.GetLocalizedText(LocalizeTextCategory.ShopItem, shopItem), shopItem == selectedShopItem))
-                    selectedShopItem = shopItem;
+                if (!ImGui.Selectable(GameInterop.GetLocalizedText(LocalizeTextCategory.ShopItem, shopItem), shopItem == selectedShopItem)) continue;
+                selectedShopItem = shopItem;
+                enabled = false;
             }
             ImGui.EndCombo();
         }
